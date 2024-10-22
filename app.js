2c50158a4d098a798aafdbd1127f87e44a6b332b
Adam Guthrie <adma.c.guthrie@diamondgradelabs.com>
2024-10-22 00:46:51 -0500

    Added POST with Body Test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const server = express()
+server.use(express.json())
 const port = 3000
 
 server.get('/', (req,res) => {
@@ -10,7 +11,18 @@ server.get('/', (req,res) => {
 server.post('/simple', (req,res) => {
 	console.log("POST /simple Resposne")
 	res.send("Simple Post");
-})		
+})
+
+server.post('/complex', (req,res) => {
+	console.log("POST /complex Response");
+	console.log(req.body);
+	if(req.body && req.body.name == "John") {
+		res.send("Complex Post");
+	}
+	else {
+		res.status(400).send("Bad Request");
+	}
+})
 
 server.listen(port, () => {
 	console.log("Testing Server Started")
